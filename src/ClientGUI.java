32410485f189554b5eb959c1e5140036deff7dee
Jewsus Christ <gyrevsk8@gmail.com>
2022-12-06 18:09:20 +0300

    gui 4

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -63,7 +63,7 @@ public class ClientGUI extends JFrame {
         {
            messege+=input.getText();
            textArea.setText( textArea.getText() + "<p>" + input.getText());
-           scrollPane.setViewport(scrollPane.setViewport(););
+
             System.out.println(textArea.getText());
            System.out.println(messege);
            Client.currentCommand = messege;
