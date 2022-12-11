2d63cb401f5ba3dda6b4b2a828eab98e6d7df03c
Jewsus Christ <gyrevsk8@gmail.com>
2022-12-11 13:24:53 +0300

    gui 5.0

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -5,36 +5,49 @@ import java.awt.image.BufferedImage;
 
 public class ClientGUI extends JFrame {
 
+    protected boolean light = true;
     JButton button = new JButton("Enter");
+    JButton outfit = new JButton("☽");
     JTextField input = new JTextField("",15);
-    JLabel textArea = new JLabel("DFSDFSDFGSDFG");
+    public JLabel textArea = new JLabel("DFSDFSDFGSDFG");
 
     JLabel johnsnow = new JLabel();
     String messege = "";
     final JScrollPane scrollPane = new JScrollPane(textArea);
 
+
+
+
     public ClientGUI()
     {
 
         super("Client ");
-        ImageIcon icon = new ImageIcon("C:\\Users\\Jewsus Christ\\IdeaProjects\\ChatCosSan\\src\\wifi.png");
+        ImageIcon icon = new ImageIcon("butt.png");
         icon = new ImageIcon(icon.getImage().getScaledInstance(300,150, BufferedImage.SCALE_SMOOTH));
 
      //   textArea.setOpaque(false);
         this.setBounds(150, 150, 480, 260);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         textArea.setText("<html>");
-        //this.setIconImage(new javax.swing.ImageIcon("C:\\Users\\Jewsus Christ\\IdeaProjects\\ChatCosSan\\src\\wp.jpg").getImage());
-        //input.setBackground(Color.GRAY);
+        textArea.setBackground(Color.lightGray);
+        textArea.setOpaque(true);
+
+        outfit.setBackground(Color.BLACK);
+        outfit.setForeground(Color.white);
+        outfit.setFocusPainted(false);
+        outfit.setOpaque(true);
+
+        button.setFocusPainted(false);
+
         Container container = this.getContentPane();
         scrollPane.setMinimumSize(new Dimension(280,150));
         scrollPane.setPreferredSize(new Dimension(280,160));
         scrollPane.setWheelScrollingEnabled(true);
-       container.setLayout(new GridBagLayout());
+        container.setLayout(new GridBagLayout());
 
         //textArea.setIcon(icon);
         super.setIconImage(icon.getImage());
-       GridBagConstraints constraints = new GridBagConstraints();
+        GridBagConstraints constraints = new GridBagConstraints();
         // container.setLayout(new FlowLayout(FlowLayout.));
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.weightx = 0.5;
@@ -42,14 +55,12 @@ public class ClientGUI extends JFrame {
         johnsnow.setFont(new Font("Times New Roman", Font.BOLD, 16));
         textArea.setFont(new Font("Times New Roman", Font.BOLD, 18));
         constraints.gridwidth = 2;
-        constraints.gridy = 0;
         constraints.gridx = 2;
         container.add(scrollPane,constraints);
 
         constraints.gridx = 2;
         constraints.gridy = 4;
-        johnsnow.setBackground(Color.BLUE);
-        johnsnow.setForeground(Color.PINK);
+
         johnsnow.setPreferredSize(new Dimension(280,40));
         container.add(johnsnow,constraints);
 
@@ -62,9 +73,19 @@ public class ClientGUI extends JFrame {
         constraints.weightx   = 0.0;
         constraints.gridwidth = 1;    // размер кнопки в две ячейки
         constraints.gridx     = 4;    // нулевая ячейка по горизонтали
+        constraints.gridy     = 0;    // первая ячейка по вертикали
+        container.add(outfit, constraints);
+
+
+        constraints.ipady     = 1;   // кнопка высокая
+        constraints.ipadx = 1;
+        constraints.weightx   = 0.0;
+        constraints.gridwidth = 1;    // размер кнопки в две ячейки
+        constraints.gridx     = 4;    // нулевая ячейка по горизонтали
         constraints.gridy     = 5;    // первая ячейка по вертикали
         container.add(button, constraints);
 
+        outfit.addActionListener(new OutfitListener());
         button.addActionListener(new ButtonListener());
         input.addKeyListener(new KeyListener() {
 
@@ -120,6 +141,64 @@ public class ClientGUI extends JFrame {
 
         }
     }
+    class OutfitListener implements ActionListener
+    {
+
+        public void actionPerformed(ActionEvent e) {
+            if (light) {
+              getContentPane().setBackground(Color.BLACK);
+              textArea.setBackground(Color.DARK_GRAY);
+              textArea.setOpaque(true);
+
+              scrollPane.setBackground(Color.BLACK);
+              scrollPane.setOpaque(true);
+
+              button.setBackground(Color.black);
+                button.setForeground(Color.WHITE);
+              button.setOpaque(true);
+
+                johnsnow.setBackground(Color.DARK_GRAY);
+                johnsnow.setForeground(Color.WHITE);
+                johnsnow.setOpaque(true);
+
+                input.setBackground(Color.DARK_GRAY);
+                input.setForeground(Color.WHITE);
+                input.setOpaque(true);
+
+              outfit.setBackground(Color.WHITE);
+              outfit.setForeground(Color.black);
+              outfit.setText("☼");
+              outfit.setOpaque(true);
+                light = !light;
+            } else
+            {
+                getContentPane().setBackground(Color.WHITE);
+                textArea.setBackground(Color.lightGray);
+                textArea.setOpaque(true);
+
+                johnsnow.setBackground(Color.WHITE);
+                johnsnow.setForeground(Color.BLACK);
+                johnsnow.setOpaque(true);
+
+                button.setBackground(Color.WHITE);
+                button.setForeground(Color.black);
+                button.setOpaque(true);
+
+                input.setBackground(Color.WHITE);
+                input.setForeground(Color.BLACK);
+                input.setOpaque(true);
+
+                outfit.setBackground(Color.BLACK);
+                outfit.setForeground(Color.WHITE);
+                outfit.setText("☽");
+                outfit.setOpaque(true);
+                light = !light;
+            }
+
+
+        }
+    }
+
 
 
 
