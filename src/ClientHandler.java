95ddd1cd4c13a6e2b1476e3968c8aa4aca7ac6fc
Fobblified <brusnitsyn2003@mail.ru>
2022-12-16 21:14:52 +0300

    Update ClientHandler.java

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,22 +1,21 @@
-import javax.swing.*;
-import java.awt.*;
+import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
 
-public class Client {
+public class ClientHandler implements Runnable,ClientF{
 
-    private Socket socket;
-    private BufferedReader bufferedReader;
-    private BufferedWriter bufferedWriter;
-    private String username ;
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>(); // Для того чтобы прокрутить всех
+    // (для последующей отправки сообщений через BufferWriter)
 
-    private String phonenumber;
+    private Socket socket; // Для установления соединения между клиентом и сервером
+    private BufferedReader bufferedReader; // Для того, чтобы считывать отправленные сообщения
+    private BufferedWriter bufferedWriter; // Для того, чтобы отправить сообщение клиенту
+    private String clientUsername ; // Имя пользователя
+    private String clientPhone;
+    private String clientPassword;
 
-    Logger logger = new Logger();
-    // IPGet ipg = new IPGet();
-    public Client(Socket socket, String username,String phonenumber) {
+    public ClientHandler(Socket socket) {
         try {
             this.socket = socket;
 
@@ -27,68 +26,64 @@ public class Client {
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            this.username = username;
-            this.phonenumber = phonenumber;
+            this.clientUsername = bufferedReader.readLine(); // Считываем имя пользователя
+            this.clientPhone = bufferedReader.readLine();
+            this.clientPassword = bufferedReader.readLine();
+
+            DatabaseHandler dbHandler = new DatabaseHandler();
+
+            dbHandler.singUpUser(clientUsername, clientPhone, clientPassword);
+
+
+            clientHandlers.add(this); // Добавляем пользователя в массив
+            broadcastMessage("SERVER: " + clientUsername + " has entered the chat");
+            Client.gui.textArea.setText(Client.gui.textArea.getText()+"<p>"+"SERVER: " + clientUsername + " has entered the chat");
         } catch (IOException e) {
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
 
-    public String getusername()
-    {
-        return username;
-    }
-    public void sendMessage() {
-        try {
-            bufferedWriter.write(username);
-            bufferedWriter.newLine();
 
-            bufferedWriter.flush();
+    @Override
+    public void run() {
+        String messageFromClient;
 
-            Scanner scanner = new Scanner(System.in);
-            while (socket.isConnected()) {
-                Client.sleepe();
-                String messageToSend =  currentCommand;
-                bufferedWriter.write(username + ": " + messageToSend);
-                gui.textArea.setText(gui.textArea.getText()+"<p>"+username+":"+messageToSend);
-                logger.setNewLogMessage(username,phonenumber, messageToSend);
-                bufferedWriter.newLine();
-                bufferedWriter.flush();
+        while (socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
             }
-        } catch (IOException e) {
-            closeEverything(socket, bufferedReader, bufferedWriter);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
     }
 
-    public void listenForMessage() { // Нужно использовать еще один поток. Мы не будем дожидаться получения сообщения, чтобы отправить свое, мы сможем сделать это сразу
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String msgFromGroupChat;
-
-                while (socket.isConnected()) {
-                    try {
-                        msgFromGroupChat = bufferedReader.readLine(); // Считываем сообщение
-                        System.out.println(msgFromGroupChat);
-                        JScrollBar vertical = gui.scrollPane.getVerticalScrollBar();
-                        gui.textArea.setHorizontalAlignment(SwingConstants.RIGHT);
-                        gui.textArea.setOpaque(true);
-                        vertical.setValue( vertical.getMaximum() );
-                        gui.textArea.setText(gui.textArea.getText()+"<p>"+msgFromGroupChat);
-                        gui.textArea.setHorizontalAlignment(SwingConstants.LEFT);
-                        gui.textArea.setOpaque(true);
-                        vertical.setValue( vertical.getMaximum() );
-                    } catch (IOException e) {
-                        closeEverything(socket, bufferedReader, bufferedWriter);
-                    }
+    public void broadcastMessage(String messageToSend) {
+        for (ClientHandler clientHandler : clientHandlers) {// цикл for each
+            try {
+                if (!clientHandler.clientUsername.equals(clientUsername)) {
+                    clientHandler.bufferedWriter.write('\n'+messageToSend);
+                    Client.gui.textArea.setText(Client.gui.textArea.getText()+"<p>"+clientUsername+":"+messageToSend);
+                    clientHandler.bufferedWriter.newLine(); // Говорит - брат, я отправил сообщение, не нужно больше ожидать текст
+                    clientHandler.bufferedWriter.flush(); // Очищаем буфер
                 }
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
             }
-        }).start();
+        }
     }
 
-    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+    public void removeClientHandler() { // Если клиент отключился, мы его удаляем, потому, что нам больше не нужно отправлять ему сообщения
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: " + clientUsername + " has left the chat!");
+        //  Client.gui.addUserlist(Client.gui.clientUsernames,);
+        Client.gui.textArea.setText(Client.gui.textArea.getText()+"<p>"+"SERVER: " + clientUsername + " has left the chat!");
+
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        removeClientHandler();
         try {
             if (bufferedReader != null) { // Если bufferReader окажется null, выдаст нам ошибку, т.к метод close() чувтсвителен к этому. Этот кусок это правит.
                 bufferedReader.close();
@@ -103,83 +98,4 @@ public class Client {
             e.printStackTrace();
         }
     }
-
-
-    static String currentCommand = "a";
-    static boolean asd = true;
-    static ClientGUI gui = new ClientGUI();
-    public static void main(String[] args) throws Exception {
-        IPHandler iph = new IPHandler();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Manual or Auto?");
-
-
-
-        gui.johnsnow.setText("Manual or Auto?");
-        gui.setVisible(true);
-        String ip = new String();
-        sleepe();// этот метод усыпляет поток
-
-        if(currentCommand.equals("m")) {
-
-            System.out.println("Manual");
-            gui.johnsnow.setText("Manual \n IP:");
-            ip = iph.ipset();
-        }
-        if(currentCommand.equals("a"))
-        {
-            System.out.println("Auto");
-            gui.johnsnow.setText("Auto");
-            ip=iph.ipautoset();
-            System.out.println("Autodetected ip: "+ip);
-            gui.johnsnow.setText("Autodetected ip: "+ip);
-        }
-
-
-        System.out.println("IP seted");//отладка
-        gui.johnsnow.setText("IP seted");
-        Socket socket = new Socket(ip, 1234);
-
-
-        System.out.println("Enter your username: ");
-        gui.johnsnow.setText("Enter your username: ");
-        sleepe();
-        // String username = scanner.nextLine();
-        String username = currentCommand;
-        System.out.println(currentCommand);//отладка
-
-        System.out.println("Enter your password: ");
-        gui.johnsnow.setText("Enter your password: ");
-        sleepe();
-        String password = currentCommand;
-        System.out.println(currentCommand);//отладка
-
-        System.out.println("Enter your phone: ");//отладка  //
-        gui.johnsnow.setText("Enter your phone: ");
-        sleepe();
-
-        // String phonenumber = scanner.nextLine();
-        String phonenumber = currentCommand;
-        String phonenew = Phone.checkPhone(phonenumber);
-
-        DatabaseHandler dbHandler = new DatabaseHandler();
-
-        dbHandler.singUpUser(username, phonenew, password);
-
-        gui.johnsnow.setText("");
-        Client client = new Client(socket, username,phonenew);
-        client.listenForMessage();
-        client.sendMessage();
-    }
-
-    static void sleepe() throws InterruptedException {
-        while(asd)
-        {
-            TimeUnit.SECONDS.sleep(1);
-        }
-        JScrollBar vertical = gui.scrollPane.getVerticalScrollBar();
-        vertical.setValue( vertical.getMaximum() );
-        asd = true;//после его использования всегда должен стоять asd=true;
-    }
-
 }
