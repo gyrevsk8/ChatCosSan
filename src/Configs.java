0894bcc78850ddeec6944ff0e2c7c2c0d3b77d4b
Fobblified <brusnitsyn2003@mail.ru>
2022-12-20 01:20:48 +0300

    Update Configs.java

--- src/Configs.java
+++ src/Configs.java
@@ -4,4 +4,8 @@ public class Configs {
     protected String dbUser = "root";
     protected String dbPass = "root";
     String dbName = "chat";
+
+    // Поля и методы, обозначенные модификатором доступа protected, будут видны:
+    //в пределах всех классов, находящихся в том же пакете, что и наш;
+    // в пределах всех классов-наследников нашего класса.
 }
