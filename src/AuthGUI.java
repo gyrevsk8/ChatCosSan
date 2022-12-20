61dabbf77f6781e15af90ca37022af97c9fccbbf
Jewsus Christ <gyrevsk8@gmail.com>
2022-12-20 23:51:59 +0300

    6.5 login

--- src/AuthGUI.java
+++ src/AuthGUI.java
@@ -4,35 +4,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AuthGUI extends JFrame {
-    JLabel userSign = new JLabel("Username");
-    JLabel passwordSign = new JLabel("Password");
-    JButton buttonLeft = new JButton("Sign in");
-    JButton buttonRight = new JButton("Sign up");
-    JTextField input1 = new JTextField("",15);
-    JTextField input2 = new JTextField("",15);
-    Container container = this.getContentPane();
-    GridBagConstraints constraints = new GridBagConstraints();
+    JButton buttonLeft = new JButton("Sign in");//создание кнопок выбора
+    JButton buttonRight = new JButton("Sign up");//создание кнопок выбора
+
+    Container container = this.getContentPane();//контейнер содержащий объекты
+    GridBagConstraints constraints = new GridBagConstraints();//создание канфигуратора для сетки
     public AuthGUI() {
-        super("Authorization");
-        this.setBounds(860, 400, 250, 150);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super("Authorization");//установка имени окна
+        this.setBounds(860, 400, 250, 150);//установка метса и размеров окна
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//операция при закрытии окна
 
-        buttonLeft.setPreferredSize(new Dimension(100,23));
-        buttonRight.setPreferredSize(new Dimension(100,23));
-        container.setLayout(new GridBagLayout());
-        constraints.fill = GridBagConstraints.HORIZONTAL;
+        buttonLeft.setPreferredSize(new Dimension(100,23));//установка желаемых размеров кнопок
+        buttonRight.setPreferredSize(new Dimension(100,23));//установка желаемых размеров кнопок
+        container.setLayout(new GridBagLayout());//установка сетки GridBag
+        constraints.fill = GridBagConstraints.HORIZONTAL;//устанавливает для элементов растяжку по горизонтали
 
-        constraints.gridwidth = 1;
-        constraints.gridy = 1;
-        constraints.gridx = 2;
+        constraints.gridwidth = 1;//ширина 1 клетка
+        constraints.gridy = 1;//координата по у
+        constraints.gridx = 2;//координата по х
         container.add(buttonLeft,constraints);
-        constraints.gridy = 1;
-        constraints.gridx = 4;
+        constraints.gridy = 1;//координата по у
+        constraints.gridx = 4;//координата по х
         container.add(buttonRight,constraints);
 
-        constraints.gridy = 2;
-        constraints.gridx = 1;
-        constraints.gridwidth = 4;
+        constraints.gridy = 2;//координата по у
+        constraints.gridx = 1;//координата по х
+        constraints.gridwidth = 4;//ширина 4 клетки
         //container.add(input,constraints);
         container.revalidate();
 
