86762135f75144aaaf94fae3ee4364dbd2279d88
Jewsus Christ <gyrevsk8@gmail.com>
2022-12-23 01:08:35 +0300

    no comments to tests

--- src/ClientTest.java
+++ src/ClientTest.java
@@ -1,13 +1,13 @@
 import java.net.Socket;
 
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTest {
 
-  //  @org.junit.jupiter.api.Test
+   @org.junit.jupiter.api.Test
     void testsendMessage() {
         Socket socket = new Socket();
         Client client = new Client(socket, "who", "phone", "pass","su");
-    //    assertEquals(null, client.getName());
+       assertEquals(null, client.getName());
     }
 }
